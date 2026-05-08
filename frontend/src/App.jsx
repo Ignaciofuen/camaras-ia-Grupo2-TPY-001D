0e782ab0747b05a0619de5c877465dc954bcb1b3
Mambee73 <nicolas.arcos.alfaro@gmail.com>
2026-05-07 20:33:55 -0400

    refactor(app): integrar AuthProvider y estructura principal de la aplicación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
-import AppRouter from "./app/router";
+import AppRouter from './app/router';
+import { AuthProvider } from './auth/AuthProvider';
 
 function App() {
-  return <AppRouter />;
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  );
 }
 
 export default App;
